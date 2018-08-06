770ff9ad6d6153e7d6c9fb065ffda4a49763eb4d
Vikas Dhiman <dhiman@umich.edu>
2018-08-06 18:16:36 -0400

    more debugging on qsub side

--- setup.sh
+++ setup.sh
@@ -1,13 +1,13 @@
 . /etc/profile.d/modules.sh
-echo "shell is:"
-echo $0
-echo "--------"
 THISDIR=$(dirname $(readlink -m "${BASH_SOURCE[0]}"))
 export MID_DIR=/z/home/dhiman/mid/
 . $THISDIR/envsetup/*.sh
 MCMD=/z/sw/Modules/$MODULE_VERSION/bin/modulecmd
 eval `$MCMD bash use /z/home/dhiman/wrk/common/modulefiles/`
 eval `$MCMD bash load miniconda3/4.5.1 cuda/8.0.61 cudnn/8.0-v6.0 numpy/py3.6/1.14.0 opencv/3.4.0 matplotlib/py3.6/2.1.2 ipython/py3.6/6.2.1 pytorch/py3.6/0.4.1 pytorch/py3.6/0.4.1`
+echo "MODULEPATH is:"
+echo $MODULEPATH
+echo "--------"
 export PROJECT_NAME=floyd_warshall_rl
 PIPDIR=$MID_DIR/$PROJECT_NAME/build/
 PYPATH=$PIPDIR/lib/python3.6/site-packages/
@@ -22,6 +22,3 @@ MJPATH=$HOME/.mujoco/mjpro150/bin
 if [[ "$LD_LIBRARY_PATH" != *"$MJPATH"* ]]; then
     export LD_LIBRARY_PATH=$MJPATH:$LD_LIBRARY_PATH
 fi
-qsub() {
-    $(which qsub) -j oe -o $MID_DIR/$PROJECT_NAME/qsub_\$PBS_JOBID.out $*
-}
