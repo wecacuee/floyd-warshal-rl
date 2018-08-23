08ab589e5ad6556080e4330baa129acb60a59a40
Vikas Dhiman <dhiman@umich.edu>
2018-08-23 00:51:31 -0400

    added pbs script for new executable

---
+++ pbs/fwr-qlnet-scrolling-play.sh
@@ -0,0 +1,23 @@
+#PBS -N fwrl-tab-algs-grid-world-play          # Any name to identify your job
+#PBS -j oe                   # Join error and output files for convinience
+#PBS -l walltime=1000:00:00     # Keep walltime big enough to finish the job
+#PBS -l nodes=1:ppn=1 # nodes requested: Processor per node: gpus requested
+#PBS -S /bin/bash            # Shell to use
+#PBS -m a                  # Mail to <user>@umich.edu on abort, begin and end
+#PBS -M dhiman@umich.edu     # Email id to alert
+#PBS -o /z/home/dhiman/mid/floyd_warshall_rl/pbs/$PBS_JOBID.out
+#
+# #PBS -q fluxg              # Not required for blindspot but for flux
+# #PBS -A jjcorso_fluxg      # Not required for blindspot but for flux
+
+echo "starting pbs script"
+VERSION=0.1.0
+MID_DIR=/z/home/dhiman/mid/floyd_warshall_rl/
+for d in src-$VERSION pbs build; do mkdir -p $MID_DIR/$d; done
+cd $MID_DIR/src-$VERSION/
+git clone $MID_DIR/git/ floyd_warshall_rl
+cd floyd_warshall_rl
+git pull
+. setup.sh
+fwrl-qlnet-scrolling-play
+echo "end of pbs script"
